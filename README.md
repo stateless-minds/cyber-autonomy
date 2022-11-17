0c807e8cfaab92301535b6b58020ad20067df49f
mar1n3r0 <m.kirkov@gmail.com>
2022-11-17 14:24:23 +0200

    add code

---
+++ README.md
@@ -0,0 +1,230 @@
+
+  
+
+  
+
+# Cyber Autonomy
+
+  
+
+  
+
+  
+
+![Logo](./assets/logo.png)
+
+  
+
+
+We are increasingly living in a centralized, totalitarian society where we end up atomized instead of empowered autonomous individuals. What's more the intellectual elite which has always produced alternative ideas is fruitless to define a new path ahead and unusually silent. The world-known philosophers of the 19th and 20th centuries are a distant memory with nothing truly new at the horizon that reflects technological progress and overall human development.
+
+Cyber Autonomy is the fruit of a year of applied research and a couple of years of conceptualization. It summarizes the abstracts of autonomy across the pillars of our society -  economics, politics and media and offers viable alternatives to the status quo. At its core it focuses on the autonomy of the individual as a stepping stone for a vital global community. The goal is to bring back autonomy to the forefront of discussions and to attract the attention of the academic circles.
+
+  
+
+  
+
+## Screenshots
+
+  
+
+  
+
+  
+
+<a  display="inline"  href="./assets/cyber-stasis.png?raw=true">
+
+  
+
+  
+
+<img  src="./assets/cyber-stasis.png"  width="45%"  alt="Screenshot of Cyber Stasis"  title="Screenshot of Cyber Stasis">
+
+  
+
+  
+
+</a>
+
+  
+
+  
+
+  
+
+<a  display="inline"  href="./assets/cyber-acid.png?raw=true">
+
+  
+
+  
+
+<img  src="./assets/cyber-acid.png"  width="45%"  alt="Screenshot of Cyber Acid"  title="Screenshot of Cyber Acid">
+
+  
+
+  
+
+</a>
+
+  
+
+  
+
+  
+
+<a  display="inline"  href="./assets/cyber-witness.png?raw=true">
+
+  
+
+  
+
+<img  src="./assets/cyber-witness.png"  width="45%"  alt="Screenshot of Cyber Witness"  title="Screenshot of Cyber Witness">
+
+  
+
+  
+
+</a>
+
+
+
+  
+
+## Community
+
+  
+
+  
+
+https://www.reddit.com/r/CyberAutonomy/
+
+  
+
+  
+
+## How to Play
+
+  
+
+  
+
+  
+
+The website runs on the public IPFS network. In order to play it follow the steps below:
+
+  
+
+  
+
+  
+
+1. Install the official IPFS Desktop http://docs.ipfs.io/install/ipfs-desktop/
+
+  
+
+  
+
+2. Install IPFS Companion http://docs.ipfs.io/install/ipfs-companion/
+
+  
+
+  
+
+3. Clone https://github.com/stateless-minds/kubo to your local machine, build it with `make build` and run it with the following command: `~/cmd/ipfs/ipfs daemon --enable-pubsub-experiment`
+
+
+4. Navigate to <a  href="https://ipfs.io/ipfs/Qma65fjuZDW1tcfFqseGWoHCCgEbGpyJ8a7ddzytbtF3zf">https://ipfs.io/ipfs/Qma65fjuZDW1tcfFqseGWoHCCgEbGpyJ8a7ddzytbtF3zf</a> and let's simulate the future together!
+
+  
+
+  
+
+5. If you like the idea consider pinning it to your local node so that you become a permanent host of it while you have IPFS daemon running
+
+  
+
+  
+
+![SetPinning](./assets/pin.png)
+
+  
+
+  
+
+![PinToLocalNode](./assets/pin-to-local-node.png)
+
+  
+
+  
+
+  
+
+Please note the website has been developed on a WQHD resolution(2560x1440) and is currently not responsive or optimized for mobile devices. For best gaming experience if you play in FHD(1920x1080) please set your browser zoom settings to 150%.
+
+## Acknowledgments
+
+  
+
+  
+
+  
+
+1.  <a  href="https://go-app.dev/">go-app</a>
+
+  
+
+  
+
+2.  <a  href="https://ipfs.io/">IPFS</a>
+
+  
+
+  
+
+3.  <a  href="https://berty.tech/">Berty</a>
+
+  
+
+  
+
+4. All the rest of the authors who worked on the dependencies used! Thanks a lot!
+
+  
+
+  
+
+  
+
+## Contributing
+
+  
+
+  
+
+  
+
+<a  href="https://github.com/stateless-minds/cyber-autonomy/issues">Open an issue</a>
+
+  
+
+  
+
+  
+
+## License
+
+  
+
+  
+
+  
+
+Stateless Minds (c) 2022 and contributors
+
+  
+
+  
+
+  
+
+MIT License
---
+++ cyber-autonomy.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+	"github.com/maxence-charriere/go-app/v9/pkg/app"
+)
+
+// pubsub is a component that does a simple pubsub on ipfs. A component is a
+// customizable, independent, and reusable UI element. It is created by
+// embedding app.Compo into a struct.
+type autonomy struct {
+	app.Compo
+}
+
+// The Render method is where the component appearance is defined. Here, a
+// "pubsub World!" is displayed as a heading.
+func (a *autonomy) Render() app.UI {
+	return app.Div().Class("l-application").Role("presentation").Body(
+		app.Link().Rel("stylesheet").Href("https://assets.ubuntu.com/v1/vanilla-framework-version-3.8.0.min.css"),
+		app.Link().Rel("stylesheet").Href("https://use.fontawesome.com/releases/v6.2.0/css/all.css"),
+		app.Link().Rel("stylesheet").Href("/app.css"),
+		app.Section().Class("p-strip--image is-light").Body(
+			app.Div().Class("row u-vertically-center").Body(
+				app.Div().Class("col-6").Body(
+					app.H1().Text("Cyber Autonomy"),
+					app.P().Text("P2P self-governance society prototype researching the intersection of moneyless economy, liquid democracy and p2p media."),
+					app.Button().Text("Read the paper").OnClick(a.openModal),
+				).Style("max-width", "fit-content!important"),
+				app.Div().Class("col-6 u-hide--small u-align--center").Body(
+					app.Img().Src("web/self-governance.png").Alt(""),
+				).Style("max-width", "fit-content!important"),
+			),
+		),
+		app.Section().Class("p-strip--image is-light").Body(
+			app.Div().Class("row u-vertically-center").Body(
+				app.Div().Class("col-6").Body(
+					app.H1().Text("Cyber Stasis"),
+					app.P().Text("Post-money economy simulator in the form of a free fictional game based on gift economy that tests the hypothesis of having a market system without any exchange be it barters or money."),
+					app.A().Href("https://ipfs.io/ipfs/QmVbgUuNz28nmMn63spZRydzDQneLewwUCGLtZGBnSEx8U").Text("Try now"),
+				).Style("max-width", "fit-content!important"),
+				app.Div().Class("col-6 u-hide--small u-align--center").Body(
+					app.Img().Src("web/cyber-stasis.png").Alt(""),
+				).Style("max-width", "fit-content!important"),
+			),
+		),
+		app.Section().Class("p-strip--image is-light").Body(
+			app.Div().Class("row u-vertically-center").Body(
+				app.Div().Class("col-6").Body(
+					app.H1().Text("Cyber Acid"),
+					app.P().Text("Liquid democracy political simulator based on the automated data feed from the moneyless economy simulator Cyber Stasis."),
+					app.A().Href("https://ipfs.io/ipfs/QmUGE4LKpRboFi2wNRi35588gRKpgM22R5k96ab46jbK9g").Text("Try now"),
+				).Style("max-width", "fit-content!important"),
+				app.Div().Class("col-6 u-hide--small u-align--center").Body(
+					app.Img().Src("web/cyber-acid.png").Alt(""),
+				).Style("max-width", "fit-content!important"),
+			),
+		),
+		app.Section().Class("p-strip--image is-light").Body(
+			app.Div().Class("row u-vertically-center").Body(
+				app.Div().Class("col-6").Body(
+					app.H1().Text("Cyber Witness"),
+					app.P().Text("Decentralized media simulator based on the reporter and witnesses concept. It aims to be an alternative to mass media, surveillance, censorship, fact checkers and centralized control of news."),
+					app.A().Href("https://ipfs.io/ipfs/Qmb2sKM6j3zgBe8MfLfFHmEecrqEeT3JGZxWWzk5GFNYNs").Text("Try now"),
+				).Style("max-width", "fit-content!important"),
+				app.Div().Class("col-6 u-hide--small u-align--center").Body(
+					app.Img().Src("web/cyber-witness.png").Alt(""),
+				).Style("max-width", "fit-content!important"),
+			),
+		),
+		app.Div().Class("p-modal").ID("paper-modal").Style("display", "none").Body(
+			app.Section().Class("p-modal__dialog").Role("dialog").Aria("modal", true).Aria("labelledby", "modal-title").Aria("describedby", "modal-description").Body(
+				app.Header().Class("p-modal__header").Body(
+					app.H2().Class("p-modal__title").ID("modal-title").Text("Why do we need autonomy"),
+					app.Button().Class("p-modal__close").Aria("label", "Close active modal").Aria("controls", "modal").OnClick(a.closeModal),
+				),
+				app.Div().Class("p-heading-icon--small").Body(
+					app.Aside().Class("p-accordion").Body(
+						app.Ul().Class("p-accordion__list").Body(
+							app.Li().Class("p-accordion__group").Body(
+								app.Div().Role("heading").Aria("level", "3").Class("p-accordion__heading").Body(
+									app.Button().Type("button").Class("p-accordion__tab").ID("tab1").Aria("controls", "tab1-section").Aria("expanded", true).Text("Introduction").Value("tab1-section").OnClick(a.toggleAccordion),
+								),
+								app.Section().Class("p-accordion__panel").ID("tab1-section").Aria("hidden", false).Aria("labelledby", "tab1").Body(
+									app.P().Text("Any system based on growth gets bigger and more centralized the more advanced it becomes. This happens naturally in the form of optimizations and convenience. Think of the transition from grocery stores to supermarkets and malls. Centralization makes scalability, planning and predictability easier. The downside is that individuals lose autonomy."),
+								),
+							),
+							app.Li().Class("p-accordion__group").Body(
+								app.Div().Role("heading").Aria("level", "3").Class("p-accordion__heading").Body(
+									app.Button().Type("button").Class("p-accordion__tab").ID("tab2").Aria("controls", "tab2-section").Aria("expanded", true).Text("How do we get back autonomy without losing automation and scalability").Value("tab2-section").OnClick(a.toggleAccordion),
+								),
+								app.Section().Class("p-accordion__panel").ID("tab2-section").Aria("hidden", true).Aria("labelledby", "tab1").Body(
+									app.P().Text("The most advanced technology we have to date to solve the problem is peer-to-peer technology. It allows for individuals to interact directly and maintain a horizontal structure."),
+								),
+							),
+							app.Li().Class("p-accordion__group").Body(
+								app.Div().Role("heading").Aria("level", "3").Class("p-accordion__heading").Body(
+									app.Button().Type("button").Class("p-accordion__tab").ID("tab3").Aria("controls", "tab3-section").Aria("expanded", true).Text("What can autonomy do and what it can't").Value("tab3-section").OnClick(a.toggleAccordion),
+								),
+								app.Section().Class("p-accordion__panel").ID("tab3-section").Aria("hidden", true).Aria("labelledby", "tab3").Body(
+									app.P().Text("Autonomy is basically individual freedom. The freedom to decide everything on a personal level and to also participate in global decision making equally with all other participants. Autonomy is the opposite of control and as such relies on learning from mistakes rather than preventing them from happening."),
+								),
+							),
+							app.Li().Class("p-accordion__group").Body(
+								app.Div().Role("heading").Aria("level", "3").Class("p-accordion__heading").Body(
+									app.Button().Type("button").Class("p-accordion__tab").ID("tab4").Aria("controls", "tab4-section").Aria("expanded", true).Text("Where is autonomy applicable").Value("tab4-section").OnClick(a.toggleAccordion),
+								),
+								app.Section().Class("p-accordion__panel").ID("tab4-section").Aria("hidden", true).Aria("labelledby", "tab4").Body(
+									app.P().Text("Practically everywhere you rely on an institution, organization, structure or a governing body to make decisions on your behalf."),
+								),
+							),
+							app.Li().Class("p-accordion__group").Body(
+								app.Div().Role("heading").Aria("level", "3").Class("p-accordion__heading").Body(
+									app.Button().Type("button").Class("p-accordion__tab").ID("tab5").Aria("controls", "tab4-section").Aria("expanded", true).Text("What happens if we lose all our autonomy").Value("tab5-section").OnClick(a.toggleAccordion),
+								),
+								app.Section().Class("p-accordion__panel").ID("tab5-section").Aria("hidden", true).Aria("labelledby", "tab5").Body(
+									app.P().Text("We will effectively be enslaved as individuals. We will still have a facade of rights but they will not be practiced effectively in reality."),
+								),
+							),
+							app.Li().Class("p-accordion__group").Body(
+								app.Div().Role("heading").Aria("level", "3").Class("p-accordion__heading").Body(
+									app.Button().Type("button").Class("p-accordion__tab").ID("tab6").Aria("controls", "tab6-section").Aria("expanded", true).Text("Support us").Value("tab6-section").OnClick(a.toggleAccordion),
+								),
+								app.Section().Class("p-accordion__panel").ID("tab6-section").Aria("hidden", true).Aria("labelledby", "tab6").Body(
+									app.A().Href("https://opencollective.com/stateless-minds-collective").Text("https://opencollective.com/stateless-minds-collective"),
+								),
+							),
+							app.Li().Class("p-accordion__group").Body(
+								app.Div().Role("heading").Aria("level", "3").Class("p-accordion__heading").Body(
+									app.Button().Type("button").Class("p-accordion__tab").ID("tab7").Aria("controls", "tab7-section").Aria("expanded", true).Text("Terms of service").Value("tab7-section").OnClick(a.toggleAccordion),
+								),
+								app.Section().Class("p-accordion__panel").ID("tab7-section").Aria("hidden", true).Aria("labelledby", "tab7").Body(
+									app.Div().Class("p-card").Body(
+										app.H3().Text("Introduction"),
+										app.P().Class("p-card__content").Text("Cyber Autonomy is a full P2P self-governance society prototype researching the intersection of moneyless economy, liquid democracy and p2p media. By using the application you are implicitly agreeing to share your peer id with the IPFS public network."),
+									),
+									app.Div().Class("p-card").Body(
+										app.H3().Text("Application Hosting"),
+										app.P().Class("p-card__content").Text("Cyber Autonomy is a decentralized website and is hosted on a public peer to peer network. By using the application you agree to host it on the public IPFS network free of charge for as long as your usage is."),
+									),
+									app.Div().Class("p-card").Body(
+										app.H3().Text("External Links"),
+										app.P().Class("p-card__content").Text("Cyber Autonomy contains links to its sister projects Cyber Stasis, Cyber Acid and Cyber Witness and depends on their availability to function properly."),
+									),
+								),
+							),
+							app.Li().Class("p-accordion__group").Body(
+								app.Div().Role("heading").Aria("level", "3").Class("p-accordion__heading").Body(
+									app.Button().Type("button").Class("p-accordion__tab").ID("tab8").Aria("controls", "tab7-section").Aria("expanded", true).Text("Privacy policy").Value("tab8-section").OnClick(a.toggleAccordion),
+								),
+								app.Section().Class("p-accordion__panel").ID("tab8-section").Aria("hidden", true).Aria("labelledby", "tab8").Body(
+									app.Div().Class("p-card").Body(
+										app.H3().Text("Personal data"),
+										app.P().Class("p-card__content").Text("There is no personal information collected within Cyber Autonomy. We store a small portion of your peer ID encrypted as a non-unique identifier which is used for displaying the ranks interface."),
+									),
+									app.Div().Class("p-card").Body(
+										app.H3().Text("Coookies"),
+										app.P().Class("p-card__content").Text("Cyber Autonomy does not use cookies."),
+									),
+									app.Div().Class("p-card").Body(
+										app.H3().Text("Changes to this privacy policy"),
+										app.P().Class("p-card__content").Text("This Privacy Policy might be updated from time to time. Thus, it is advised to review this page periodically for any changes. You will be notified of any changes from this page. Changes are effective immediately after they are posted on this page."),
+									),
+								),
+							),
+						),
+					),
+				),
+			),
+		),
+	).Style("display", "block").Style("overflow-y", "scroll")
+}
+
+func (a *autonomy) openModal(ctx app.Context, e app.Event) {
+	app.Window().GetElementByID("paper-modal").Set("style", "display:flex")
+}
+
+func (a *autonomy) closeModal(ctx app.Context, e app.Event) {
+	app.Window().GetElementByID("paper-modal").Set("style", "display:none")
+}
+
+func (a *autonomy) toggleAccordion(ctx app.Context, e app.Event) {
+	id := ctx.JSSrc().Get("value").String()
+	attr := app.Window().GetElementByID(id).Get("attributes")
+	aria := attr.Get("aria-hidden").Get("value").String()
+	if aria == "false" {
+		app.Window().GetElementByID(id).Call("setAttribute", "aria-hidden", "true")
+	} else {
+		app.Window().GetElementByID(id).Call("setAttribute", "aria-hidden", "false")
+	}
+}
